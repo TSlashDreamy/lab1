2f0c53f01581442728261d16b344db449bb916bd
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2023-02-25 01:38:17 +0200

    Class transferred to a separate file

---
+++ lab1/Counter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public partial class Counter
+    {
+        private const int min = -99;
+        private const int max = 99;
+
+        private int count;
+        public int Count
+        {
+            get { return count; }
+            set
+            {
+                // ternary operator - fast solution
+                // count = value >= min && value <= max ? value : 0;
+
+                // better solution
+                if (count < min) { count = min; }
+                else if (count > max) { count = max; }
+                else { count = value; }
+            }
+        }
+
+        public Counter()
+        {
+            count = 0;
+        }
+
+        /// <summary>
+        /// Increases the counter by a predetermined amount
+        /// </summary>
+        /// <param name="amount"></param>
+        public void increaseAmount(int amount)
+        {
+            Count += amount;
+        }
+
+        /// <summary>
+        /// Decreases the counter by a predetermined amount
+        /// </summary>
+        /// <param name="amount"></param>
+        public void decreaseAmount(int amount)
+        {
+            Count -= amount;
+        }
+
+        /// <summary>
+        /// Increases counter by 1 
+        /// </summary>
+        public void increase()
+        {
+            Count++;
+        }
+
+        /// <summary>
+        /// Decreases counter by 1 
+        /// </summary>
+        public void decrease()
+        {
+            Count--;
+        }
+    }
+}
