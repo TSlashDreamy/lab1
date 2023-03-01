e535f6a93c181d67692d87e9b910c3ef751d8538
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2023-03-01 11:26:26 +0200

    exception handler added

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -4,26 +4,32 @@
 Counter newCounter = new Counter();
 Console.WriteLine(newCounter.Count);
 
-// increase by amount and show
-newCounter.countManagement(5);
-Console.WriteLine(newCounter.Count);
+try
+{
+    // increase by amount and show
+    newCounter.countManagement(5);
+    Console.WriteLine(newCounter.Count);
 
-// increase by 1 and show
-newCounter.increase();
-Console.WriteLine(newCounter.Count);
+    // increase by 1 and show
+    newCounter.increase();
+    Console.WriteLine(newCounter.Count);
 
-// decrease by amount and show
-newCounter.countManagement(-3);
-Console.WriteLine(newCounter.Count);
+    // decrease by amount and show
+    newCounter.countManagement(-3);
+    Console.WriteLine(newCounter.Count);
 
-// decrease by 1 and show
-newCounter.decrease();
-Console.WriteLine(newCounter.Count);
+    // decrease by 1 and show
+    newCounter.decrease();
+    Console.WriteLine(newCounter.Count);
 
-// trying to set to high value (throws exception)
-newCounter.countManagement(100); // 99 is max limit
-Console.WriteLine(newCounter.Count);
+    // trying to set to high value (throws exception)
+    newCounter.countManagement(100); // 99 is max limit
+    Console.WriteLine(newCounter.Count);
 
-// trying to set to low value (throws exception)
-newCounter.countManagement(-200); // -99 is min limit
-Console.WriteLine(newCounter.Count);
+    // trying to set to low value (throws exception)
+    newCounter.countManagement(-200); // -99 is min limit
+    Console.WriteLine(newCounter.Count);
+} catch (Exception e)
+{
+    Console.WriteLine($"Exception catched: {e}");
+}
